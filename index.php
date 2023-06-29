2ee69af54ef023dc7bc054b206302d13bc6a1528
MVC <78438743+MVC1453@users.noreply.github.com>
2023-06-29 17:47:17 +0300

    Update index.php

--- index.php
+++ index.php
@@ -16,43 +16,44 @@
         die('Failed to connect to MySQL: ' . mysqli_connect_error());
     }
 
-    // Fetch blog posts from the database
-    $query = 'SELECT * FROM blog_posts';
-    $result = mysqli_query($conn, $query);
+    // Check if a specific blog post is requested
+    if (isset($_GET['post'])) {
+        $postId = $_GET['post'];
 
-    if ($result && mysqli_num_rows($result) > 0) {
-        // Check if a specific blog post is requested
-        if (isset($_GET['post'])) {
-            $postId = $_GET['post'];
+        // Fetch the individual blog post from the database
+        $query = "SELECT * FROM blog_posts WHERE id = $postId";
+        $result = mysqli_query($conn, $query);
 
-            // Fetch the individual blog post from the database
-            $query = "SELECT * FROM blog_posts WHERE id = $postId";
-            $result = mysqli_query($conn, $query);
+        if ($result && mysqli_num_rows($result) > 0) {
+            $post = mysqli_fetch_assoc($result);
 
-            if ($result && mysqli_num_rows($result) > 0) {
-                $post = mysqli_fetch_assoc($result);
-
-                // Display the individual blog post content
-                echo '<h2>' . $post['title'] . '</h2>';
-                echo '<p>' . $post['content'] . '</p>';
-                echo '<p>Author: ' . $post['author'] . '</p>';
-                echo '<p>Date: ' . $post['date'] . '</p>';
-                echo '<a href="index.php">Return to Blog List</a>';
-            } else {
-                echo '<h2>Post not found.</h2>';
-            }
+            // Display the individual blog post content
+            echo '<h2>' . $post['title'] . '</h2>';
+            echo '<p>' . $post['content'] . '</p>';
+            echo '<p>Author: ' . $post['author'] . '</p>';
+            echo '<p>Date: ' . $post['date'] . '</p>';
+            echo '<a href="index.php">Return to Blog List</a>';
         } else {
-            // Display the list of blog posts
+            echo '<h2>Post not found.</h2>';
+        }
+    } else {
+        // Fetch blog posts from the database
+        $query = 'SELECT * FROM blog_posts';
+        $result = mysqli_query($conn, $query);
+
+        if ($result && mysqli_num_rows($result) > 0) {
+            // Display the list of blog posts with author and date
             echo '<h1>My Blog</h1>';
 
             while ($row = mysqli_fetch_assoc($result)) {
                 echo '<h2><a href="?post=' . $row['id'] . '">' . $row['title'] . '</a></h2>';
-                echo '<p>' . $row['content'] . '</p>';
+                echo '<p>Author: ' . $row['author'] . '</p>';
+                echo '<p>Date: ' . $row['date'] . '</p>';
                 echo '<hr>';
             }
+        } else {
+            echo '<h2>No blog posts found.</h2>';
         }
-    } else {
-        echo '<h2>No blog posts found.</h2>';
     }
 
     // Close the database connection
